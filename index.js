1def2ea0310af405e7a4601a626fd1e21ddc8ac6
Nik Butenko <nik@butenko.me>
2014-01-06 18:42:02 +1100

    Complete refactoring, remove "singleton" config

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-module.exports = process.env.STATIC_BOOTSTRAP_COV ? require('./lib-cov/dispatcher') : require('./lib/dispatcher');
+var _ = require('underscore');
+var errors = require('./lib/errors');
+var router = require('./lib/router');
+var http = require('./lib/http');
+var Dispatcher = require('./lib/dispatcher');
+
+
+var defaults = {
+	root: null,
+	renderer: null,
+	jsonp: '__jsonp',
+	noError: '__noError',
+	defaultController: 'index',
+	defaultAction: 'index'
+};
+
+
+function configure(_options) {
+	var options = _.defaults(_options, defaults);
+	
+	
+	
+	
+	
+	
+	return {
+		route: route,
+		defaultRoute: defaultRoute
+	};
+	
+//exports.errors = errors;
+//exports.route = route;
+//exports.defaultRoute = defaultRoute;
+//exports.http = require('./http');
+}
+
+configure.http = http;
+configure.errors = errors;
+
+
+module.exports = exports = configure;
